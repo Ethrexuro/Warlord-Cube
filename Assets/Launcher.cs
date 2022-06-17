ec38c88a7b4e54cf52586e62051bf830aed1ff5c
Andrew N. Tran <andrewtran3131@protonmail.com>
2022-06-17 16:05:47 +1000

    Photon Code.
    Updates and fixes.

---
+++ Assets/Launcher.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace com.MyCompany.MyGame
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+{
+    #region Private Serializable Fields
+
+    #endregion
+
+    #region Private Fields
+
+    string gameVersion = "1";
+
+    #endregion
+
+    #region  MonoBehaviour CallBacks
+
+    void Awake(){
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start(){
+        Connect();
+    }
+
+    #endregion
+
+    #region Public Fields
+
+    public void Connect(){
+        if (PhotonNetwork.IsConnected){
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    #endregion
+
+    #region MonobahviourPunCallbacks Callbacks
+
+    public override void OnConnectedToMaster(){
+        Debug.LogWarningFormat("OnConnectedToMaster() was called by PUN");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause){
+        Debug.LogWarningFormat("OnDissconnected() was called by PUN with reason {0}", cause);
+    }
+
+    #endregion
+
+    public override void OnJoinRandomFailed(short returnCode, string message){
+        Debug.Log("Successfully Create Room");
+            PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+
+    public override void OnJoinedRoom(){
+        Debug.Log("Successfully joined Room");
+    }
+}
+
+}
